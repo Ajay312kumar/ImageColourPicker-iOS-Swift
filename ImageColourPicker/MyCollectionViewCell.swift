13d6d0a288b6babf50ccccf3bbe9de74103816fa
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-02-17 11:24:31 +0530

    upload image
    upload image and pick it's colour

---
+++ ImageColourPicker/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  ImageColourPicker
+
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var backgroundColorView: UIView!
+    
+    
+    
+}
